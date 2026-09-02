fea4a29e2d722c7987de3ab08dd94326692b1652
ne_moroz <percovkaman@gmail.com>
2026-09-02 12:52:50 +0300

    6.5 Фикстуры Pytest

--- tests/conftest.py
+++ tests/conftest.py
@@ -33,7 +33,9 @@ def initialize_browser_state(playwright: Playwright) -> None:
 
 
 @pytest.fixture(scope="function")
-def chromium_page_with_state(playwright: Playwright) -> None:
+def chromium_page_with_state(
+        initialize_browser_state, playwright: Playwright
+) -> Page:
     browser = playwright.chromium.launch(headless=False)
     context = browser.new_context(storage_state="browser-state.json")
     page = context.new_page()
